e5bfd15a9136a9c202da2bf574aba58cd69a1532
Mohamed Magdy <moh.magdy40@gmail.com>
2017-04-19 01:15:35 +0200

    [ADD] Add two models for the Employees and Departments and register it to the admin backend.

--- apps/hr_employee/admin.py
+++ apps/hr_employee/admin.py
@@ -3,4 +3,28 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import Employee, Department
 # Register your models here.
+
+
+## Employees
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Employee, EmployeeAdmin)
+
+
+## Departments
+class EmployeeInline(admin.TabularInline):
+    model = Employee
+    extra = 3
+
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'manager']
+    fieldsets = [
+        (None, {'fields': ['name']}),
+        ('More...', {'fields':['manager', ], 'classes':['collapse']})
+    ]
+
+admin.site.register(Department, DepartmentAdmin)
--- apps/hr_employee/models.py
+++ apps/hr_employee/models.py
@@ -4,3 +4,21 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    name = models.CharField(verbose_name='Employee Name', null=False, blank=False, max_length=64, )
+
+
+    def __str__(self):
+        return self.name
+
+
+class Department(models.Model):
+    name = models.CharField(verbose_name='Department Name', null=False, blank=False, max_length=64, )
+    manager = models.ForeignKey(Employee, )
+
+    def __str__(self):
+        return self.name
+
+
+Employee.department = models.ForeignKey(Department, )
