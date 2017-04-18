19c0a1e14c52c7f44ddf22c6be8e687fb1901e2d
Mohamed Magdy <moh.magdy40@gmail.com>
2017-04-19 00:11:03 +0200

    [ADD] Add the URLs for the main pages.

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
 from . import views
 
+app_name = 'base'
+
 urlpatterns = [
-    url(r'^', views.index),
+    url(r'^', views.index, name='main'),
 ]
--- apps/hr_employee/urls.py
+++ apps/hr_employee/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, include
 from . import views
 
+app_name = 'hr_employee'
+
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index, name='main'),
+    # url(r'^employees/$', views.employees, name='employees'),
 ]
