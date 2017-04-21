c02162914b9ffb821f25e26b13cabb61357f331f
Mohamed Magdy <moh.magdy40@gmail.com>
2017-04-21 15:14:05 +0200

    [ADD] Add the employee's contacts.

--- apps/hr_employee/models.py
+++ apps/hr_employee/models.py
@@ -7,7 +7,12 @@ from django.db import models
 
 class Employee(models.Model):
     name = models.CharField(verbose_name='Employee Name', null=False, blank=False, max_length=64, )
-
+    personal_phone = models.CharField(verbose_name='Personal Phone', max_length=28, )
+    personal_mobile = models.CharField(verbose_name='Personal Mobile', max_length=28, )
+    personal_email = models.EmailField(verbose_name='Personal Email', )
+    work_phone = models.CharField(verbose_name='Work Phone', max_length=28, )
+    work_mobile = models.CharField(verbose_name='Work Mobile', max_length=28, )
+    work_email = models.EmailField(verbose_name='Work Email', )
 
     def __str__(self):
         return self.name
