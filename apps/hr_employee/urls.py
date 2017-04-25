ebab15a3fc1002edda5ee8db0b97eff1a6d25ca0
Mohamed Magdy <moh.magdy40@gmail.com>
2017-04-26 01:27:25 +0200

    [ADD] Add a template, URL and a view for the employee creation.

--- apps/hr_employee/urls.py
+++ apps/hr_employee/urls.py
@@ -8,6 +8,7 @@ app_name = 'hr_employee'
 urlpatterns = [
     # url(r'^$', views.index, name='main'),
     url(r'^employees/form/(?P<pk>[0-9]+)', views.EmployeeDetailView.as_view(), name='employees_form'),
+    url(r'^employees/create/', views.EmployeeCreateView.as_view(), name='employees_create'),
     url(r'^employees/', views.EmployeeIndexView.as_view(), name='employees'),
     url(r'^departments/form/(?P<pk>[0-9]+)', views.DepartmentDetailView.as_view(), name='departments_form'),
     url(r'^departments/', views.DepartmentIndexView.as_view(), name='departments'),
--- apps/hr_employee/views.py
+++ apps/hr_employee/views.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+from django.urls import reverse
 from django.views import generic
 from .models import Employee, Department
 
@@ -15,6 +17,15 @@ class EmployeeIndexView(generic.ListView):
 
 class EmployeeDetailView(generic.DetailView):
     model = Employee
+
+
+class EmployeeCreateView(generic.CreateView):
+    model = Employee
+    fields = ['name', 'work_phone', 'work_mobile']
+    template_name_suffix = '_create_form'
+
+    def get_success_url(self):
+        return reverse('hr_employee:employees')
 
 
 # Departments
